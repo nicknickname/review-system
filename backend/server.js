cb0b5920f4577a662751c254c1fbd4014ff679cf
GitHubNail <nv222ch@student.lnu.se>
2019-11-10 22:06:20 +0100

    MERN set up

--- backend/server.js
+++ backend/server.js
@@ -10,15 +10,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-/*
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
-);
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
-})
-*/
 // Set up mongoose connection
 var mongoDB = process.env.MONGODB_URI
 mongoose.connect(mongoDB, { useNewUrlParser: true, useCreateIndex: true }) // Because DeprecationWarning
